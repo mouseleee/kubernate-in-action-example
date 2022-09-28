e021e793ff5277c58fb68342af27c2aad79e571c
mouselee <mouse.tree.lee@gmail.com>
2022-09-28 18:27:36 +0800

    add

--- volume/fortuneloop-env.sh
+++ volume/fortuneloop-env.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 trap "exit" SIGINT
-INTERVAL=$1
 echo Configured to generate new fortune every $INTERVAL seconds
-mkdir /var/htdocs
+mkdir -p /var/htdocs
 while :
 do
   echo $(date) Writing fortune to /var/htdocs/index.html
